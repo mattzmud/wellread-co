2a7276921dd7328db75337aeed84bd4b98108c06
mattzmud <matt.zmud@gmail.com>
2026-07-27 23:12:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 // ⚠️ Bump this version number every time you deploy changes.
 // This forces the old cache to be cleared and fresh files fetched.
-const CACHE_NAME    = "wellread-v29";
+const CACHE_NAME    = "wellread-v30";
 const STATIC_ASSETS = [
   "/",
   "/index.html",
